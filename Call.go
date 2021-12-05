6316b6a212639680a33caf438f1205e1c9006257
Marc <marc.park@gmail.com>
2021-12-05 23:33:33 +0900

    Test from desktop

--- Call.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func duh() {
-	fmt.Println("Hello world2!")
-}
---
+++ Main.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello world2!")
+// }
+
+import ("fmt"
+"marc/pkg1")
+
+func duh() {
+	fmt.Println("Hello world2!")
+}
+
+func main() {
+	duh()
+	m1.Test1()
+}
